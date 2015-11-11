b418d30352c0823e94ff55a8cd3aa6bed6c6ec6c
Sebastian Kaim <sebb@sebb767.de>
2015-11-11 10:12:36 +0100

    Basic code

---
+++ zserv/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace zserv
+{
+	/// <summary>
+	/// Contains some helper functions.
+	/// </summary>
+	public static class Helper
+	{
+		/// <summary>
+		/// Incorporates the specified objects into self.
+		/// </summary>
+		/// <param name="self">The string to incorporate into.</param>
+		/// <param name="format">The objects to enter.</param>
+		public static string incorporate(this string self, params object[] insert)
+		{
+			int i = 0;
+			return string.Format(Regex.Replace(self,"%.",m=>("{"+ ++i +"}")), insert);
+		}
+	}
+}
+
---
+++ zserv/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zserv
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/* parse command line option
+			 * -d deattach (start subprocess and tunnel output, exit on [deattached]
+			 * -c config file
+			 * -o global options
+			 **/
+		}
+	}
+}
