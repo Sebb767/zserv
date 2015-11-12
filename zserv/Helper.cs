0306caec1d7f75b06b92eb31466bb5086e48952b
Sebastian Kaim <sebb@sebb767.de>
2015-11-12 08:14:27 +0100

    Added each() helper function

--- zserv/Helper.cs
+++ zserv/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace zserv
 {
@@ -17,6 +18,47 @@ namespace zserv
 		{
 			int i = 0;
 			return string.Format(Regex.Replace(self,"%.",m=>("{"+ ++i +"}")), insert);
+		}
+
+		/// <summary>
+		/// Executes action for each element in self.
+		/// </summary>
+		/// <param name="self">A list of T.</param>
+		/// <param name="action">An action to call for every T.</param>
+		public static void each<T>(this IList<T> self, Action<T> action)
+		{
+			foreach (T t in self)
+				action (t);
+		}
+
+		/// <summary>
+		/// Executes action for each element in self.
+		/// </summary>
+		/// <param name="self">A list of T.</param>
+		/// <param name="action">An action to call for every T.</param>
+		/// <returns>The count of elements in self.</returns>
+		public static int each<T>(this IList<T> self, Action<T, int> action)
+		{
+			int i = 0;
+			foreach (T t in self)
+				action (t, i++);
+
+			return i;
+		}
+
+		/// <summary>
+		/// Executes action for each element in self.
+		/// </summary>
+		/// <param name="self">A list of T.</param>
+		/// <param name="action">An action to call for every T.</param>
+		/// <returns>The count of elements in self.</returns>
+		public static int each<T>(this IList<T> self, Action<int, T> action)
+		{
+			int i = 0;
+			foreach (T t in self)
+				action (i++, t);
+
+			return i;
 		}
 	}
 }
